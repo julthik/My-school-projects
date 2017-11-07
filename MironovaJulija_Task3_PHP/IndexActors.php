cb31aa328a74ffc97071081f5281a3944049ba4b
Julija Mironova <julthik.mironova@gmail.com>
2017-11-07 11:21:42 +0200

    Added comments to index.php and indexActors.php

--- MironovaJulija_Task3_PHP/IndexActors.php
+++ MironovaJulija_Task3_PHP/IndexActors.php
@@ -48,55 +48,62 @@ require_once "autoloader.php";//Благодаря содержимому autolo
                     <div class="col-md-12 offset-md-0">
 
 
+                        <!-- Навигация по сайту-->
                         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="category" class="collapse navbar-collapse" id="navbarNavDropdown">
-                                <ul class="navbar-nav">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="category" class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
 
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Category
-                                        </a>
-                                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                            <?php
-                                            $db=new PDOService();
-                                            //$db=new MySQLiService();
-                                            foreach ($db->getAllCategories() as $category) {
-                                            ?>
-                                            <a class="dropdown-item" href="Index.php?send=<?php echo $category->getId(); ?>" name="send"> <?php echo $category->name ?></a>
-                                            <?php
-                                            }
-                                            ?>
-                                        </div>
-                                    </li>
+                                <!-- Выпадающий список с элементами категории-->
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Category
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <?php
+                                        //Инициализация $db
+                                        $db=new PDOService();
+                                        //$db=new MySQLiService();
+                                        foreach ($db->getAllCategories() as $category) {//Через цикл foreach перебирается весь массив
+                                        ?>
+                                        <!-- Передача id категории через адресную строку и вывод названий категорий в выпадающем списке на экран-->
+                                        <a class="dropdown-item" href="Index.php?send=<?php echo $category->getId(); ?>" name="send"> <?php echo $category->name ?></a>
+                                        <?php
+                                        }
+                                        ?>
+                                    </div>
+                                </li>
 
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="IndexActors.php">Actors</a>
-                                    </li>
+                                <!-- Ссылка на страницу актёры-->
+                                <li class="nav-item">
+                                    <a class="nav-link" href="IndexActors.php">Actors</a>
+                                </li>
 
-                                </ul>
-                            </div>
-                        </nav>
+                            </ul>
+                        </div>
+                    </nav>
 
 
                         <?php 
-                        foreach ($db->getAllActors() as $actor) {
-                        $id=$actor->id;?>
+                        foreach ($db->getAllActors() as $actor) {//Через цикл foreach перебирается весь массив
+                        $id=$actor->id;//$id присвается id автора?>
                         <div class="panel-group" id="collapse-group">
                             <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <h4 class="panel-title">
+                                    <!-- Кнопка с именем и фамилией актёра-->
                                         <a data-toggle="collapse" data-parent="#collapse-group" href="#<?php echo $actor->id; ?>"><?php echo "<H4 style='color:#000000'><b>".$actor->firstname." ".$actor->lastname."</H4>"; ?></a>
                                     </h4>
                                 </div>
-                                <div id="<?php echo $actor->id; ?>" class="panel-collapse collapse">
+                                <div id="<?php echo $actor->id; ?>" class="panel-collapse collapse"><!-- Получение id актера-->
                                     <div class="panel-body">
                                         <?php
-                                        foreach ($db->getAllFilmsInfo() as $film) {
-                                            foreach($film->actors as $f){
-                                                if($id==$f->id){
+                                        foreach ($db->getAllFilmsInfo() as $film) {//Через цикл foreach перебирается весь массив
+                                            foreach($film->actors as $f){//Перебираем вложенный массив с актерами
+                                                if($id==$f->id){//Если полученный id равен id автора
+                                                    //Выводим на экран фильмы данного актера
                                                     echo "<H4 style='color:#17a2b8'><b>".$film->title.", ".$film->releaseYear."</b></H4><H5>".$film->description."</H5><br /><br />";
                                                 }
                                             }
--- MironovaJulija_Task3_PHP/index.php
+++ MironovaJulija_Task3_PHP/index.php
@@ -47,70 +47,78 @@ require_once "autoloader.php";//Благодаря содержимому autolo
                 <div class="row">
                     <div class="col-md-12 offset-md-0">
 
+                        <!-- Навигация по сайту-->
                         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="category" class="collapse navbar-collapse" id="navbarNavDropdown">
-                                <ul class="navbar-nav">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="category" class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
 
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Category
-                                        </a>
-                                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                            <?php
-                                            $db=new PDOService();
-                                            //$db=new MySQLiService();
-                                            foreach ($db->getAllCategories() as $category) {
-                                            ?>
-                                            <a class="dropdown-item" href="Index.php?send=<?php echo $category->getId(); ?>" name="send"> <?php echo $category->name ?></a>
-                                            <?php
-                                            }
-                                            ?>
-                                        </div>
-                                    </li>
+                                <!-- Выпадающий список с элементами категории-->
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Category
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <?php
+                                        //Инициализация $db
+                                        $db=new PDOService();
+                                        //$db=new MySQLiService();
+                                        foreach ($db->getAllCategories() as $category) {//Через цикл foreach перебирается весь массив
+                                        ?>
+                                        <!-- Передача id категории через адресную строку и вывод названий категорий в выпадающем списке на экран-->
+                                        <a class="dropdown-item" href="Index.php?send=<?php echo $category->getId(); ?>" name="send"> <?php echo $category->name ?></a>
+                                        <?php
+                                        }
+                                        ?>
+                                    </div>
+                                </li>
 
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="IndexActors.php">Actors</a>
-                                    </li>
+                                <!-- Ссылка на страницу актёры-->
+                                <li class="nav-item">
+                                    <a class="nav-link" href="IndexActors.php">Actors</a>
+                                </li>
 
-                                </ul>
-                            </div>
-                        </nav>
+                            </ul>
+                        </div>
+                    </nav>
 
 
                         <?php 
-                    if(isset($_REQUEST['send'])){
-                        $id = $_REQUEST['send'];
-                        $control=0;
-                        foreach ($db->getAllFilmsInfo() as $film) {
-                            if ($film->hasCategory($id)) {
+                    if(isset($_REQUEST['send'])){// if(isset($_REQUEST['send'])) Если были получены данные из адресной строки отличные от NULL, то выводится информация о фильме
+                        $id = $_REQUEST['send'];//$id присвается переданное значение ($id категории)
+                        $control=0;//Пременная создана для того, что бы выводить на экран информацию о том, что в категории фильмы отсутствуют
+                        foreach ($db->getAllFilmsInfo() as $film) {//Проходимся с помощью foreach по массиву $film
+                            if ($film->hasCategory($id)) {//Если данная категория существует, то 
+                                    //На экран выводится название фильма, дата выхода фильма и описание к нему
                                     echo "<p><H4 style='color:#17a2b8'><b>".$film->title.", ".$film->releaseYear."</b></H4><br />".$film->description."<br /></p>";
                                     echo "<p><b>Categories</b></p>";
-                                    foreach($film->categories as $c){
-                                        echo "<H5>".$c->name." </H5>";
+                                    foreach($film->categories as $c){//Перебираем вложенный массив 
+                                        echo "<H5>".$c->name." </H5>";//Вывод на экран всех жанров фильма
                                     } 
                                     echo "<p><b><br />Actors</b></p>";
-                                    $counter=1;
-                                    foreach($film->actors as $f){
-                                        $result = count($film->actors);
-                                            if($result>$counter){
-                                                $counter=$counter+1;
+                                    $counter=1;//Пременная создана для того, что бы красиво выводить фамилии и имена актёров через запятую
+                                    foreach($film->actors as $f){//Перебираем вложенный массив 
+                                        $result = count($film->actors);//Узнаем количество актеров фильма
+                                            if($result>$counter){//Если кол-во актёров больше $counter
+                                                $counter=$counter+1;//Прибавляем ещё одного актера
+                                                //и выводим на экран имя, фамилию актёра и запятую
                                                 echo "<H5>".$f->firstname." ".$f->lastname.", </H5>";
-                                            }else{
+                                            }else{//иначе
+                                                //выводим на экран имя и фамилию актёра без запятой
                                                 echo "<H5>".$f->firstname." ".$f->lastname."</H5>";
                                             } 
                                     } 
-                                    $control=$control+1;
+                                    $control=$control+1;//Добавляем к $control единицу, значит фильмы в данной категории есть
                                     echo "<br /><br />";
                             }
                         } 
-                        if($control==0)  {
+                        if($control==0)  {//Если $control равен 0, то фильмы в данной категории отсутствуют
                             echo "<H2 align='center'>So far there are no movies. Choose another category.</H2>";
                         } 
-                        $control=0;
-                }else{
+                        $control=0;//Обнуляем $control
+                }else{//if(isset($_REQUEST['send'])) иначе выводится на экран, что нужно выбрать категорию или актёра
                         ?><H2 align="center"><?php echo "To see a list of movies, select the category or actor that interests you";?></H2><?php
                     }?>
                     </div>
