99551f70d021482b72cce4b0e70a683bdc3de835
Julija Mironova <julthik.mironova@gmail.com>
2017-12-27 14:47:16 +0200

    Added button

--- MironovaJulija_Task4_PHP/resources/views/pages/show.blade.php
+++ MironovaJulija_Task4_PHP/resources/views/pages/show.blade.php
@@ -6,4 +6,5 @@
 <p style="margin-top: 2em;"><h4>{{$showOneNews->description}}</h4></p>
 <h5><br/>{{$showOneNews->pubDate}}</h5>
 <h5><br/>Link: <a href="{{$showOneNews->link}}" target="_blank">{{$showOneNews->link}}</a></h5>
+<p style="margin-top: 2em;"><a href="{{ action('NewsController@index')}}" class="btn btn-info" role="button">Back</a></p>
 @endsection
